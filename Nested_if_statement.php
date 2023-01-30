cb46fe7992babdd95f354a5be8f2254f902fecdf
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-01-30 22:59:17 +0530

    Nested_if_statement

---
+++ Nested_if_statement.php
@@ -0,0 +1,14 @@
+<?php
+$age=23;
+$nationality = "Indian";
+
+if ($nationality == "Indian"){
+if($age>=18){
+    echo " you are eligible to give vote";
+}
+else {    
+    echo "Not eligible to give vote"; 
+}
+}
+
+?>
