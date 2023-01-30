2bc33e01f8a970ba4ee595ad4c8cfcf253353713
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-01-30 23:01:25 +0530

    switch_statement

---
+++ switch_statement.php
@@ -0,0 +1,24 @@
+<?php
+$num=20;
+switch($num){
+case 10:
+echo ("number is equl to 10");
+break;
+case 20:
+echo ("number is equl to 20");
+break;
+case 30:
+echo ("number is equl to 30");
+break;
+case 40:
+echo ("number is equl to 40");
+break;
+case 50:
+echo ("number is equl to 50");
+break;
+default:   
+echo ("number is not equl to 10,30,40 and 50");
+}
+?>
+
+
