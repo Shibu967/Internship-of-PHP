215aaa3e4a9d46d7181726c9ea889ea595fa4f7b
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-01-30 22:34:37 +0530

    variable
    declaration of variable

---
+++ var.php
@@ -0,0 +1,8 @@
+
+        <?php  
+            $color = "black";  
+            echo "My car is ". $ColoR ."</br>";  
+            echo "My dog is ". $color ."</br>";  
+            echo "My Phone is ". $COLOR ."</br>";  
+        ?>  
+    
