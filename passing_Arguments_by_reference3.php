25ebe25730bcce270e287210aeee228cf8991382
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-01-31 23:44:44 +0530

    passing_Arguments_by_reference3

---
+++ passing_Arguments_by_reference3.php
@@ -0,0 +1,14 @@
+<?php
+function first(&$num){
+    $num+=5;
+
+}
+function second(&$num){
+
+    $num+=7;
+}
+$number=10;
+first($number);
+echo"Original value is $numbe<br>;
+second($number);
+echo"Original value is $numbe<br>;
