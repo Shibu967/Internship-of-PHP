487a6fbc7f19c0ca787ce7f5caebbce89f8ccbc8
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-01-31 23:39:59 +0530

    function_with_parameter

---
+++ function_with_parameter.php
@@ -0,0 +1,27 @@
+<?php
+function Additions($a,$b){
+
+    $c=$a+$b;
+    echo " Addition of two numbers:$c <br>";
+}
+
+function Subtraction($a,$b){
+
+    $c=$a-$b;
+    echo " Subtraction of two numbers:$c <br>";
+}
+
+
+Additions(34,67);
+Additions(38,87);
+Additions(88,88);
+Subtraction(100,50);
+Subtraction(1000000,5);
+Subtraction(100000000000,50);
+
+
+
+
+
+
+?>
