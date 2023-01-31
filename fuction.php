1f8a8987591531b669ad9e79f3133fceeaa16100
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-01-31 23:39:28 +0530

    simple function

---
+++ fuction.php
@@ -0,0 +1,16 @@
+<?php
+function Elsner(){
+$name="Hey shibu kumari,<br>";
+$elsner="Welcome to elsner technology";
+echo" $name.$elsner <br> <br>";
+
+}
+
+Elsner();
+Elsner();
+Elsner();
+Elsner();
+
+
+
+?>
