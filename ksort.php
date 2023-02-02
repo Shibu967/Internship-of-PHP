c388fcd813be49b61f9f2daa02990e0657c5d86a
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-02-02 23:22:09 +0530

    ksort

---
+++ ksort.php
@@ -0,0 +1,14 @@
+<?php
+$salary=array(
+    "shibu"=>50000,
+    "sonu"=>20000,
+    "vipul"=>10000,
+    "simran"=>1000000);
+    ksort($salary);
+
+foreach($salary as $key=>$value){
+    echo $key .":". $value;
+    echo "</br>"; 
+}
+?>
+//sort associative arrays in ascending order, according to the key
