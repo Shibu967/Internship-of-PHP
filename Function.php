a39a5e94f3d583c9a690139d9f098589155c8e47
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-02-02 23:25:58 +0530

    addcslashes Function

---
+++ Function.php
@@ -0,0 +1,14 @@
+<?php
+$str2 = "Ram eats mango";
+echo addcslashes($str2,"R");
+echo "<br>";
+echo addcslashes($str2,"o");
+echo "<br>";
+echo addcslashes($str2,"a..z");
+echo "<br>";
+echo addcslashes($str2,"a..m");
+//PHP addcslashes() Function
+
+
+
+?>
