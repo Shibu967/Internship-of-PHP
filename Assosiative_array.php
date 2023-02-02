f40f1317bf4126381c06db13efe4f48e8abab45b
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2023-02-02 23:20:29 +0530

    Assosiative_array

---
+++ Assosiative_array.php
@@ -0,0 +1,14 @@
+<?php
+$salary=array(
+    "shibu"=>50000,
+    "sonu"=>20000,
+    "vipul"=>10000,
+    "simran"=>1000000);
+
+foreach($salary as $key=>$value){
+    echo $key .":". $value;
+    echo "</br>"; 
+}
+
+
+?>
